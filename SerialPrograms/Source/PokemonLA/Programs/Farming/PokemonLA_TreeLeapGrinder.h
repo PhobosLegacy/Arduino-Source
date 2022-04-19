ee591033d0dcc4ed2a851597202613ea29271bc4
Phobos <rotoraci@gmail.com>
2022-04-19 08:51:47 +0100

    WIP for Leap Grinder plus Fixes on previous programs

--- SerialPrograms/Source/PokemonLA/Programs/Farming/PokemonLA_TreeLeapGrinder.h
+++ SerialPrograms/Source/PokemonLA/Programs/Farming/PokemonLA_TreeLeapGrinder.h
@@ -1,51 +1,58 @@
-/*  Alpha Froslass Hunter
+/*  Tree Leap Farmer
  *
  *  From: https://github.com/PokemonAutomation/Arduino-Source
  *
  */
 
-#ifndef PokemonAutomation_PokemonLA_FroslassFinder_H
-#define PokemonAutomation_PokemonLA_FroslassFinder_H
+#ifndef PokemonAutomation_PokemonLA_TreeLeapGrinder_H
+#define PokemonAutomation_PokemonLA_TreeLeapGrinder_H
+
 #include "CommonFramework/Notifications/EventNotificationsTable.h"
 #include "NintendoSwitch/Framework/NintendoSwitch_SingleSwitchProgram.h"
 #include "PokemonLA/Options/PokemonLA_ShinyDetectedAction.h"
 #include "PokemonLA/Inference/PokemonLA_MountDetector.h"
 #include "PokemonLA/Inference/PokemonLA_UnderAttackDetector.h"
+#include "CommonFramework/Options/SimpleIntegerOption.h"
 
 namespace PokemonAutomation{
 namespace NintendoSwitch{
 namespace PokemonLA{
 
-class FroslassFinder_Descriptor : public RunnableSwitchProgramDescriptor{
+class TreeLeapGrinder_Descriptor : public RunnableSwitchProgramDescriptor{
 public:
-    FroslassFinder_Descriptor();
+    TreeLeapGrinder_Descriptor();
 };
 
-class FroslassFinder : public SingleSwitchProgramInstance{
+class TreeLeapGrinder : public SingleSwitchProgramInstance{
 public:
-    FroslassFinder(const FroslassFinder_Descriptor& descriptor);
+    TreeLeapGrinder(const TreeLeapGrinder_Descriptor& descriptor);
 
     virtual std::unique_ptr<StatsTracker> make_stats() const override;
     virtual void program(SingleSwitchProgramEnvironment& env, BotBaseContext& context) override;
 
 private:
     void run_iteration(SingleSwitchProgramEnvironment& env, BotBaseContext& context);
+    bool check_tree(SingleSwitchProgramEnvironment& env, BotBaseContext& context, int16_t match);
+    void exit_battle(BotBaseContext& context);
 
 private:
     class Stats;
     class RunRoute;
 
-    ShinyRequiresAudioText SHINY_REQUIRES_AUDIO;
-
     ShinyDetectedActionOption SHINY_DETECTED_ENROUTE;
-    ShinyDetectedActionOption SHINY_DETECTED_DESTINATION;
-
+    ShinyDetectedActionOption MATCH_DETECTED_OPTIONS;
+    SimpleIntegerOption<uint8_t> SHAKES;
+    EnumDropdownOption STOP_ON;
+    EnumDropdownOption POKEMON;
     EventNotificationOption NOTIFICATION_STATUS;
     EventNotificationsOption NOTIFICATIONS;
 };
+
+
+
+
 
 }
 }
 }
 #endif
-
